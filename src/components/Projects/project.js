8d5c67e894387015029b2f6e3df45f23a870d6dd
William Roberts <willbarronroberts@gmail.com>
2019-10-17 15:39:49 -0600

    updated project

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { TypescriptIcon } from '../Icons/typescript-icon';
+import { BabelIcon } from '../Icons/babel-icon';
+import { WebpackIcon } from '../Icons/webpack-icon';
 
-export const Project = ({ name, isPrivate, icons = [] }) => {
+export const Project = ({
+  name,
+  children,
+  about,
+  isPrivate = false,
+  icons = [],
+}) => {
   return (
-    <div className="flex flex-col items-center">
-      <h3 className={isPrivate ? 'text-red-900' : null}>{name}</h3>
-      <div className="flex items-center">
-        {icons.map((icon, index) => (
-          <div className="mx-1 text-gray-800">
-            <FontAwesomeIcon key={index} icon={['fab', icon]} size="lg" />
-          </div>
-        ))}
+    <div className="flex items-center p-8 w-1/2">
+      <div className="w-1/2"></div>
+      <div className="text-left font-body">
+        <h3 className="text-2xl font-display">{name}</h3>
+        <p>{about}</p>
+        <div className="mt-3">{children}</div>
+        <div className="flex items-center mt-3">
+          {icons.map((icon, index) => (
+            <div className="mx-1 text-gray-800">{renderIcon(icon, index)}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
+};
+
+const renderIcon = (icon, index) => {
+  const classes = ['h-5', 'w-5'];
+  const fill = '#2d3748';
+
+  switch (icon) {
+    case 'typescript':
+      return <TypescriptIcon key={index} classes={classes} fill={fill} />;
+    case 'webpack':
+      return (
+        <WebpackIcon key={index} key={index} classes={classes} fill={fill} />
+      );
+    case 'babel':
+      return <BabelIcon key={index} classes={classes} fill={fill} />;
+    default:
+      return <FontAwesomeIcon key={index} icon={['fab', icon]} size="lg" />;
+  }
 };
--- src/components/Projects/projects-section.js
+++ src/components/Projects/projects-section.js
@@ -6,38 +6,44 @@ import { Project } from './project';
 export const ProjectsSection = () => {
   return (
     <SectionContainer title="Projects">
-      <Project
-        name="Meraki Pipeline App"
-        isPrivate={true}
-        icons={[
-          'angular',
-          'node-js',
-          'js-square',
-          'salesforce',
-          'gitlab',
-          'git',
-          'html5',
-          'css3',
-        ]}
-      />
-      <div className="mt-6">
-        Meraki Pipeline App
-        <p>Private</p>
-        <p>
-          Ionic, Angular, Typescript, ngrx, rxjs, Firebase, GCP, Salesforce,
-          NoSQL, SOQL, Gitlab CI/CD, Moment
-        </p>
+      <div className="max-w-4xl m-auto flex justify-around">
+        <Project
+          name="Meraki Pipeline App"
+          isPrivate={true}
+          why="Client wanted sales reps to have easy access to their customer data without signing into Salesforce"
+          about="Full Stack Developer"
+          icons={[
+            'angular',
+            'node-js',
+            'js-square',
+            'salesforce',
+            'gitlab',
+            'git',
+            'html5',
+            'css3',
+          ]}
+        />
+        <Project
+          name="Meraki Training App"
+          isPrivate={true}
+          about="Project Lead - Frontend, Backend, Architecture"
+          icons={[
+            'angular',
+            'node-js',
+            'js-square',
+            'salesforce',
+            'gitlab',
+            'git',
+            'html5',
+            'css3',
+          ]}
+        />
+        <Project
+          name="Snake"
+          about="Weekend project to learn Typescript and get better at 2d Canvas games"
+          icons={['typescript', 'html5', 'css3', 'babel', 'webpack']}
+        />
       </div>
-
-      <div className="mt-6">
-        Meraki Training App
-        <p>Private</p>
-        <p>
-          Angular, Typescript, rxjs, Firebase, GCP, Salesforce, NoSQL, SOQL,
-          Netlify, Tailwindcss, Material, Cloudinary
-        </p>
-      </div>
-
       <div className="mt-6">
         Moon and Crater Angular Component Library
         <p>
@@ -51,14 +57,8 @@ export const ProjectsSection = () => {
         Angular Tailwind Starter Template
         <p>
           Why - Start a new project faster that already has Tailwindcss,
-          Purgecss, and autprefixer installed and ready to go.
+          Purgecss, and autoprefixer installed and ready to go.
         </p>
-      </div>
-
-      <div className="mt-6">
-        Snake
-        <p>Why - For fun!</p>
-        <p>Typescript, HTML5, Canvas, CSS3, Netlify</p>
       </div>
 
       <div className="mt-6">
